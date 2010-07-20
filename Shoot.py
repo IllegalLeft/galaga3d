d613edce5697bc6136a39635a0c15b18f550bf23
Sami Volk <samivolk16@hotmail.com>
2010-07-20 14:26:12 -0400

    Restricted bullets to 1 per second and increased bullet speed

--- Shoot.py
+++ Shoot.py
@@ -1,23 +1,28 @@
 #Shooting Script for Galaga3D
+# 2010
+# Isaac and Sami Volk
+
 #Intro
 cont = GameLogic.getCurrentController()
 
 #Sensors
 laser = cont.sensors["fire"]
 
-#Actuator
+#Actuators
 # get actuator attached to the controller named Spawn
 spawn = cont.actuators["spawn"]
 # tell AddObject actuator which object is to be added
 spawn.object = "laser"
 # set initial linear velocity
-spawn.linearVelocity = [0,10,0]
+spawn.linearVelocity = [0,20,0]
 # set life span in frames
 spawn.time = 100
 
 
-#Action
+#Actions
 if laser.positive:
 	# add the game object
 	spawn.instantAddObject()
+	# Change states to delay state
+	cont.activate("delay_state")
 
