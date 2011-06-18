1272535c2e021c95718c48d6c35dfd094c2359e5
Isaac Volk <isaacvolk3@hotmail.com>
2011-06-18 18:53:57 -0400

    Added text file with Git commands along with any changes I've made

--- Base.py
+++ Base.py
@@ -10,12 +10,15 @@ right = cont.sensors["right"]
 motion = cont.actuators["motion"]
 
 #Set variables
+rotation = [0,0,1]
+local = 1
 speed = 0
 strafe = 2
 
 #Set left & right motions wrt strafe variable
 if left.positive:
 	speed = -strafe
+	obj.applyRotation (rotation,local)
 if right.positive:
 	speed = strafe
 
