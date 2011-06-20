d3fa916cb596575610cb0a07fc0f405017d0e876
Sami Volk <samivolk16@hotmail.com>
2011-06-20 17:44:48 -0400

    Updated Base.py

--- Base.py
+++ Base.py
@@ -9,23 +9,35 @@ right = cont.sensors["right"]
 #Motion
 motion = cont.actuators["motion"]
 
+#Grab controller object for rotation
+obj = cont.owner
+
 #Set variables
-rotation = [0,0,1]
-local = 1
+leftrot = [0,-0.5,0]
+rightrot = [0,0.5,0]
+rot = [0,0,0]
+local = 0
 speed = 0
 strafe = 2
 
 #Set left & right motions wrt strafe variable
+#WIP rotations for side-to-side movement
 if left.positive:
 	speed = -strafe
-	obj.applyRotation (rotation,local)
+#	obj.applyRotation (rot,local)
+	rot = leftrot
 if right.positive:
 	speed = strafe
+#	obj.applyRotation (rot,local)
+	rot = rightrot
 
 #Use local linear velocity
-motion.useLocalLinV = True
+motion.useLocalLinV = False
 #Set LinV to speed variable
 motion.linV = (speed,0,0)
 
 #Activate the actuator
 cont.activate(motion)
+
+#Apply rotation
+obj.applyRotation (rot,local)
